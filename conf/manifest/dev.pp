b56270409b96c5f7b3e747509cf4a99e59922871
Dustin Moore <dustin@dandamoore.com>
2012-10-09 01:41:27 -0500

    Fixed git repo clone in puppet

--- conf/manifest/dev.pp
+++ conf/manifest/dev.pp
@@ -1,18 +1,20 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
 package { 'git':
   ensure => '1:1.7.9.5-1',
 }
 
 exec { "set_git_user":
-    command => "/usr/bin/git config --global user.name 'Dustin Moore'",
-    unless => "/usr/bin/git config --global user.name | grep -c 'Dustin Moore'",
+  command => "sudo -u dustin git config --global user.name 'Dustin Moore'",
+  unless => "sudo -u dustin git config --global user.name | grep -c 'Dustin Moore'",
 }
 
 exec { "set_git_email":
-    command => "/usr/bin/git config --global user.email dustin@dandamoore.com",
-    unless => "/usr/bin/git config --global user.email | grep -c dustin@dandamoore.com",
+  command => "sudo -u dustin git config --global user.email dustin@dandamoore.com",
+  unless => "sudo -u dustin git config --global user.email | grep -c dustin@dandamoore.com",
 }
 
 exec { "get_git_repo":
-    command => "/usr/bin/git clone https://github.com/dustinmoorenet/PicList.git /home/dustin/PicList",
-    unless => "[ -d /home/dustin/PickList ] && echo 1 || echo 0"
+  command => "sudo -u dustin git clone https://github.com/dustinmoorenet/PicList.git /home/dustin/PicList",
+  unless => 'test -d /home/dustin/PicList'
 }
