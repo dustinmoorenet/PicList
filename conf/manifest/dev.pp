fdfc43f1afdcfa1f1c8b16c311c109d69ff6ab4f
Dustin Moore <dustin@dandamoore.com>
2012-10-09 00:44:43 -0500

    Added git user setup to puppet

--- conf/manifest/dev.pp
+++ conf/manifest/dev.pp
@@ -1,3 +1,13 @@
 package { 'git':
   ensure => '1:1.7.9.5-1',
 }
+
+exec { "set_git_user":
+    command => "/usr/bin/git config --global user.name 'Dustin Moore'",
+    unless => "/usr/bin/git config --global user.name | grep -c 'Dustin Moore'",
+}
+
+exec { "set_git_email":
+    command => "/usr/bin/git config --global user.email dustin@dandamoore.com",
+    unless => "/usr/bin/git config --global user.email | grep -c dustin@dandamoore.com",
+}
