ce21e255ec97f2f3679bc1a5088ea25fb4a6e2ae
Dustin Moore <dustin@dandamoore.com>
2012-12-08 09:49:37 -0600

    Added read and delete unit tests to photo

--- test/photo.js
+++ test/photo.js
@@ -1,6 +1,8 @@
 var Photo = require('../lib/photo');
 var fs = require('fs');
 
+var the_id,
+    the_photo;
 
 exports.testAdd = function(test) {
   fs.createReadStream('fixtures/Test.jpg').pipe(fs.createWriteStream('fixtures/Copy_Test.jpg'));
@@ -12,7 +14,9 @@ exports.testAdd = function(test) {
     size: 12702
   }, function(data) {
     data = data || {};
-    test.ifError(data.error);
+    test.ifError(data.error, 'The photo should have created successfully');
+
+    the_id = photo.id;
 
     test.done();
   });
@@ -21,13 +25,35 @@ exports.testAdd = function(test) {
 }
 
 exports.testRead = function(test) {
+
+  // Get photo
+  var photo = new Photo(the_id, function(err) {
+    test.ifError(err, 'The photo should have been found successfully');
+
+    test.done();
+  });
+
+  the_photo = photo;
+
   test.expect(1);
-  test.ok(true, 'This is true');
-  test.done();
 }
 
 exports.testDelete = function(test) {
+  the_photo.delete(function(err) {
+    test.ifError(err, 'The photo should have deleted successfully');
+
+    test.done();
+  });
   test.expect(1);
-  test.ok(true, 'This is true again');
-  test.done();
+}
+
+exports.testReadAgain = function(test) {
+  // Get photo
+  var photo = new Photo(the_id, function(err) {
+    test.ok(err, 'The photo should not exist');
+
+    test.done();
+  });
+
+  test.expect(1);
 }
